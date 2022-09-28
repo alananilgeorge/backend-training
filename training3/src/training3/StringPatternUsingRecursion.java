4ebf317d03f0d4f0d8c36ddbacda936fafc2481c
Alan Anil George <56294456+alananilgeorge@users.noreply.github.com>
2022-09-28 11:28:54 +0530

    string pattern using recursion completed

---
+++ training3/src/training3/StringPatternUsingRecursion.java
@@ -0,0 +1,42 @@
+package training3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class StringPatternUsingRecursion {
+	static List<Character> res = new ArrayList<Character>();
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter string pattern");
+		String str = new String(s.next());
+		pattern(str, 1);
+		res.forEach((i) -> System.out.print(i));
+
+	}
+
+	public static void pattern(String s, int num) {
+
+		int i = 0;
+		int n;
+		while (i < num) {
+			for (int j = 0; j < s.length(); j++) {
+				if (s.charAt(j) == '[') {
+					System.out.println(s.charAt(j - 1));
+					num = Character.getNumericValue(s.charAt(j - 1));
+					pattern(s.substring(j + 1, s.length()), num);
+				} else if (Character.isDigit(s.charAt(j))) {
+					continue;
+				} else if (s.charAt(j) == ']') {
+					continue;
+				} else {
+					res.add(s.charAt(j));
+				}
+				i++;
+
+			}
+		}
+	}
+
+}
