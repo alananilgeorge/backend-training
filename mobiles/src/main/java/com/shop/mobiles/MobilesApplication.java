076301d27d535f22620dd56921669cee3d9b68cf
Alan Anil George <56294456+alananilgeorge@users.noreply.github.com>
2022-10-10 00:30:44 +0530

    fixed 404 error

--- mobiles/src/main/java/com/shop/mobiles/MobilesApplication.java
+++ mobiles/src/main/java/com/shop/mobiles/MobilesApplication.java
@@ -2,8 +2,12 @@ package com.shop.mobiles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import controllers.MobileController;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses=MobileController.class)
 public class MobilesApplication {
 
 	public static void main(String[] args) {
