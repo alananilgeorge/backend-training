baebd7b75991d64ae5a65246d685abb96390d7a9
Alan Anil George <56294456+alananilgeorge@users.noreply.github.com>
2022-10-09 19:56:34 +0530

    first commit

---
+++ mobiles/src/main/java/com/shop/mobiles/MobilesApplication.java
@@ -0,0 +1,13 @@
+package com.shop.mobiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilesApplication.class, args);
+	}
+
+}
---
+++ mobiles/src/main/java/models/Brand.java
@@ -0,0 +1,26 @@
+package models;
+
+import java.util.HashMap;
+
+public class Brand {
+	private String brandName = new String();
+	private int id;
+
+	public String getBrandName() {
+		return brandName;
+	}
+
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+}
